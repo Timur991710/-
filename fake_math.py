db26995646a737a33dbd959fd624ef6a353b744d
Timur991710 <tim991710@gmail.com>
2024-09-01 00:19:13 +0800

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    if second == 0:
+        return ('Ошибка, на ноль делить нельзя')
+    else:
+        val = first / second
+        return  val
---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as divi
+from true_math import divide as di
+
+rez = divi(23, 0)
+print(rez)
+rez = di(23, 5)
+print(rez)
+rez = divi(23, 5)
+print(rez)
+rez = di(23, 0)
+print(rez)
