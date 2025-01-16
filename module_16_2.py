ef129d1b2307ef10bcfc0994adf92f5cba85ee81
Timur991710 <tim991710@gmail.com>
2025-01-16 16:01:37 +0800

    Add files via upload

--- module_16_2.py
+++ module_16_2.py
@@ -15,14 +15,15 @@ async def admin():
 
 
 @app.get('/user/{user_id}')
-async def user(user_id: int = Path(gt=1, le=100, description='Enter User ID')):
+async def user(user_id: int = Path(gt=1, le=100, description='Enter User ID', example='12')):
     return {f'Вы вошли как пользователь №{user_id}'}
 
 
 @app.get('/user/{username}/{age}')
-async def user_info(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username')],
-                    age: Annotated[int, Path(gt=18, le=120, description='Enter age')]):
+async def user_info(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',example='Timur')],
+                    age: Annotated[int, Path(gt=18, le=120, description='Enter age', example='37')]):
     return f'Информация о пользователе, Имя: {username}, Возраст: {age}'
 
 
-"""Запуск файла python -m uvicorn module_16_2:app"""
+"""Запуск файла 
+python -m uvicorn module_16_2:app"""
