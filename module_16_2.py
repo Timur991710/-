45e2961efe91c3c747125bcb7696c899f90b2ca2
Timur991710 <tim991710@gmail.com>
2025-01-16 15:55:09 +0800

    Add files via upload

---
+++ module_16_2.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome():
+    return {'Главная страница'}
+
+
+@app.get('/user/admin')
+async def admin():
+    return {'Вы вошли как администратор'}
+
+
+@app.get('/user/{user_id}')
+async def user(user_id: int = Path(gt=1, le=100, description='Enter User ID')):
+    return {f'Вы вошли как пользователь №{user_id}'}
+
+
+@app.get('/user/{username}/{age}')
+async def user_info(username: Annotated[str, Path(min_length=5, max_length=20)], age: Annotated[int, Path(gt=18, le=120)]):
+    return f'Информация о пользователе, Имя: {username}, Возраст: {age}'
+
+
+"""Запуск файла python -m uvicorn module_16_2:app"""
